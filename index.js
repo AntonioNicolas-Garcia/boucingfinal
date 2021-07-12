9b4c6473a6d3e947ccb70405213d061a26839cfb
AntonioNicolas-Garcia <80422856+AntonioNicolas-Garcia@users.noreply.github.com>
2021-07-12 05:56:28 -0500

    proyecto final para extraordinario o titulo1

---
+++ index.js
@@ -0,0 +1,323 @@
+import { ImageLocal } from "./ImageLocal.js";
+import { ImageType } from "./ImageType.js";
+import { MathImg } from "./MathImg.js";
+import { Particle } from "./particle.js";
+import { ParticleText } from "./particle.js";
+import { BouncingParticle } from "./particle.js";
+var lienzo1;
+var lienzo2;
+var lienzo4;
+var pantalla1;
+var pantalla2;
+var pantalla4;
+/* Este evento controla la forma de abrir un archivo mediante el evento de arrastrar y soltar */
+function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault(); //que no se abra en otra ventana sola la imagen
+    evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+}
+/** Variables que controla el canvas de la imagen, el primero
+ * posteriormemte se volveran arreglos cuando ya controlemos las seis ventanas de nuestro frame
+*/
+lienzo1 = document.getElementById('img1');
+pantalla1 = lienzo1.getContext("2d");
+lienzo2 = document.getElementById('img2');
+pantalla2 = lienzo2.getContext("2d");
+lienzo4 = document.getElementById('img4');
+pantalla4 = lienzo4.getContext("2d");
+var dropZone = lienzo1; //document.getElementById('img1');
+var imgLocal = new ImageLocal(pantalla1);
+imgLocal.getImage().onload = imgLocal.onload;
+var imgLocal4 = new ImageLocal(pantalla4);
+imgLocal4.getImage().onload = imgLocal4.onload;
+function convertirAGris(evt) {
+    var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+    imagenSal.imageArray2DtoData(pantalla2, MathImg.toGray(imagenSal));
+}
+function convertirANegativo(evt) {
+    var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+    imagenSal.imageArray2DtoData(pantalla2, MathImg.toNegative(imagenSal));
+}
+function convertirARojo(evt) {
+    var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+    imagenSal.imageArray2DtoData(pantalla2, MathImg.toRed(imagenSal));
+}
+function convertirAVerde(evt) {
+    var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+    imagenSal.imageArray2DtoData(pantalla2, MathImg.toGreen(imagenSal));
+}
+function convertirAAzul(evt) {
+    var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+    imagenSal.imageArray2DtoData(pantalla2, MathImg.toBlue(imagenSal));
+}
+function correccionGamma(evt) {
+    var args = prompt('Ingresa los factores de correccion Gamma, separados por coma');
+    var factores = args.split(',').map(function (elem) { return parseFloat(elem); });
+    var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+    imagenSal.imageArray2DtoData(pantalla2, MathImg.correctionGamma(imagenSal, factores));
+}
+function umbralizado(evt) {
+    var args = prompt('Ingresa el valor del umbral');
+    var umbral = parseFloat(args);
+    var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+    imagenSal.imageArray2DtoData(pantalla2, MathImg.toUmbral(imagenSal, umbral));
+}
+function desfaseX(evt) {
+    var args = prompt('Ingresa el valor del desfase en X');
+    var des = parseFloat(args);
+    var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+    imagenSal.imageArray2DtoData(pantalla2, MathImg.toDesfaceX(imagenSal, des));
+}
+function desfaseY(evt) {
+    var args = prompt('Ingresa el valor del desfase en Y');
+    var desy = parseFloat(args);
+    var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+    imagenSal.imageArray2DtoData(pantalla2, MathImg.toDesfaceX(imagenSal, desy));
+}
+function umbral2limites(evt) {
+    var args = prompt('Ingresa el rango minimo y el maximo separado por comas');
+    var rangos = args.split(',').map(function (elem) { return parseFloat(elem); });
+    var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+    imagenSal.imageArray2DtoData(pantalla2, MathImg.toUmbral2limites(imagenSal, rangos));
+}
+function changeBrightness(evt) {
+    var factor = prompt("Ingresa un valor en el rango de 0-2, como un porcentaje");
+    var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+    imagenSal.imageArray2DtoData(pantalla2, MathImg.changeBrightness(imagenSal, parseFloat(factor)));
+}
+function colorGradienteX(evt) {
+    var args = prompt("Ingresa color de Inicio y final en formato r,g,b, separados por coma");
+    var factores = args.split(',').map(function (elem) { return parseFloat(elem); });
+    var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+    imagenSal.imageArray2DtoData(pantalla2, MathImg.colorGradienteX(imagenSal, factores));
+}
+function colorGradienteY(evt) {
+    var args = prompt("Ingresa color de Inicio y final en formato r,g,b, separados por coma");
+    var factores = args.split(',').map(function (elem) { return parseFloat(elem); });
+    var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+    imagenSal.imageArray2DtoData(pantalla2, MathImg.colorGradientY(imagenSal, factores));
+}
+function opchangeContraste(evt) {
+    var argss = prompt('Ingresa un valor entre el rango de -100 a 100');
+    var valor = parseFloat(argss);
+    var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+    imagenSal.imageArray2DtoData(pantalla2, MathImg.changeContraste(imagenSal, valor));
+}
+function opgetPow(evt) {
+    var argss = prompt('Ingresa un numero ( potencia )');
+    var valor = parseFloat(argss);
+    var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+    imagenSal.imageArray2DtoDataWithResizing(pantalla2, MathImg.pow(imagenSal, valor));
+}
+function coseno(evt) {
+    var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+    imagenSal.imageArray2DtoDataWithResizing(pantalla2, MathImg.toCos(imagenSal));
+}
+function multiplicacion(evt) {
+    var argss = prompt('Ingresa un numero real');
+    var valor = parseFloat(argss);
+    var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+    imagenSal.imageArray2DtoData(pantalla2, MathImg.toMultiplication(imagenSal, valor));
+}
+function subtract(evt) {
+    var argss = prompt('Ingresa un numero real');
+    var restar = parseFloat(argss);
+    var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+    imagenSal.imageArray2DtoData(pantalla2, MathImg.toSubtract(imagenSal, restar));
+}
+function funcionSine(evt) {
+    var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+    imagenSal.imageArray2DtoDataWithResizing(pantalla2, MathImg.toSine(imagenSal));
+}
+function add(evt) {
+    var argss = prompt('Ingresa un numero real');
+    var sumar = parseFloat(argss);
+    var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+    imagenSal.imageArray2DtoData(pantalla2, MathImg.toAdd(imagenSal, sumar));
+}
+function sqrt(evt) {
+    var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+    imagenSal.imageArray2DtoDataWithResizing(pantalla2, MathImg.toSqrt(imagenSal));
+}
+function div(evt) {
+    var argss = prompt('Ingresa un numero real');
+    var dividir = parseFloat(argss);
+    if (dividir == 0) {
+        var argss = prompt('Ingresa un valor diferente de 0');
+        var dividir = parseFloat(argss);
+        var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+        imagenSal.imageArray2DtoData(pantalla2, MathImg.toDividir(imagenSal, dividir));
+    }
+    else {
+        var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+        imagenSal.imageArray2DtoData(pantalla2, MathImg.toDividir(imagenSal, dividir));
+    }
+}
+function tan(evt) {
+    var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+    imagenSal.imageArray2DtoDataWithResizing(pantalla2, MathImg.toTan(imagenSal));
+}
+function sumaImg(evt) {
+    var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+    var imagen2 = new ImageType(pantalla4, imgLocal4.getImage());
+    imagenSal.imageArray2DtoDataWithResizing(pantalla2, MathImg.addImg(imagenSal, imagen2));
+}
+//variables adicionales para el efecto rain
+var ctx = pantalla2;
+var w;
+var h;
+var numberOfParticles = 200;
+var particlesArray;
+particlesArray = new Array(0);
+var imagenSal;
+function init() {
+    //init
+    var imagenSal = new ImageType(pantalla1, imgLocal.getImage());
+    var tmp = MathImg.relativeBrightness(imagenSal);
+    w = imagenSal.getWidth();
+    h = imagenSal.getHeight();
+    for (var i = 0; i < numberOfParticles; i++) {
+        particlesArray.push(new Particle(w, h, ctx, tmp));
+    }
+}
+function animate() {
+    ctx.drawImage(imgLocal.getImage(), 0, 0, w, h);
+    ctx.globalAlpha = 0.25;
+    ctx.fillStyle = 'rgb(0,0,0)';
+    ctx.fillRect(0, 0, w, h);
+    for (var i = 0; i < particlesArray.length; i++) {
+        particlesArray[i].update();
+        particlesArray[i].draw();
+    }
+    requestAnimationFrame(animate);
+}
+function animate2() {
+    ctx.globalAlpha = 0.25;
+    ctx.fillStyle = 'rgb(0,0,0)';
+    ctx.fillRect(0, 0, w, h);
+    for (var i = 0; i < particlesArray.length; i++) {
+        particlesArray[i].update();
+        ctx.globalAlpha = particlesArray[i].getSpeed() * 0.5;
+        particlesArray[i].draw();
+    }
+    requestAnimationFrame(animate2);
+}
+function rain(evt) {
+    init();
+    animate();
+}
+function rain2(evt) {
+    init();
+    animate2();
+}
+//codigo para efecto de particulas
+var particleArray;
+var mouse = {
+    x: null,
+    y: null,
+    radius: 150
+};
+function handleMouse(e) {
+    mouse.x = e.x; // - canvasPosition.left;
+    mouse.y = e.y; // - canvasPosition.top;
+    //console.log(mouse.x, mouse.y)
+}
+function textEfects(evt) {
+    var args = prompt("Ingresa texto, tamaño de texto y coord x y y, separados por coma:");
+    var factores = args.split(','); //.map(elem => parseInt(elem));
+    pantalla1.font = 'bold  ' + factores[1] + 'px Verdana';
+    //let cadena = 
+    pantalla1.fillText(factores[0], parseInt(factores[2]), parseInt(factores[3]));
+    imagenSal = new ImageType(pantalla1, null, 300, 300, true);
+    initParticles();
+    animateParticles();
+}
+function initParticles() {
+    particleArray = [];
+    var arrImage = imagenSal.getArrayImg();
+    for (var i = 0; i < 300; i++) {
+        for (var j = 0; j < 300; j++) {
+            if (arrImage[0][i][j] > 128) {
+                particleArray.push(new ParticleText(j, i, pantalla1));
+            }
+        }
+    }
+}
+function animateParticles() {
+    pantalla1.clearRect(0, 0, 300, 300);
+    for (var i = 0; i < particleArray.length; i++) {
+        particleArray[i].update(mouse);
+        particleArray[i].draw();
+    }
+    requestAnimationFrame(animateParticles);
+}
+lienzo1.addEventListener('mousemove', handleMouse);
+lienzo1.addEventListener("mousemove", imgLocal.drawSmallImg);
+// para efecto bouncingParticle
+var particleArrayBouncing;
+particleArrayBouncing = new Array(0); // particleArray = []
+var weigth;
+var size;
+var ctxb = pantalla1;
+function initpattern3Particle() {
+    var imagenSal = new ImageType(pantalla1, null, 300, 300, true);
+    var tmp = MathImg.relativeBrightness(imagenSal);
+    w = imagenSal.getWidth();
+    h = imagenSal.getHeight();
+    for (var i = 0; i < numberOfParticles; i++) {
+        particleArrayBouncing.push(new BouncingParticle(w, h, ctxb, tmp, weigth, size));
+    }
+}
+function animatepattern3Particle() {
+    //ctxb.globalAlpha = 0.25;
+    ctxb.fillStyle = 'rgb(0,0,255)';
+    ctxb.fillRect(0, 0, w, h);
+    for (var i = 0; i < particleArrayBouncing.length; i++) {
+        particleArrayBouncing[i].updatepattern3Particle(mouse);
+        particleArrayBouncing[i].draw();
+    }
+    requestAnimationFrame(animatepattern3Particle);
+}
+function pattern3Particle(evt) {
+    initpattern3Particle();
+    animatepattern3Particle();
+}
+document.getElementById('files').addEventListener('change', imgLocal.handleFileSelect, false);
+document.getElementById('files2').addEventListener('change', imgLocal4.handleFileSelect, false);
+dropZone.addEventListener('dragover', handleDragOver, false);
+dropZone.addEventListener('drop', imgLocal.handleFileSelect, false);
+//menu op basicas
+document.getElementById("op-gris").addEventListener('click', convertirAGris, false);
+document.getElementById("op-negativo").addEventListener('click', convertirANegativo, false);
+document.getElementById("op-rojo").addEventListener('click', convertirARojo, false);
+document.getElementById("op-verde").addEventListener('click', convertirAVerde, false);
+document.getElementById("op-azul").addEventListener('click', convertirAAzul, false);
+document.getElementById("op-gamma").addEventListener('click', correccionGamma, false);
+document.getElementById("op-umbral1").addEventListener('click', umbralizado, false);
+document.getElementById("op-umbral-2-limites").addEventListener('click', umbral2limites, false);
+document.getElementById("op-desfaseX").addEventListener('click', desfaseX, false);
+document.getElementById("op-desfaseY").addEventListener('click', desfaseY, false);
+//menu op. edicion
+document.getElementById("op-brillo").addEventListener('click', changeBrightness, false);
+document.getElementById("op-gradienteX").addEventListener('click', colorGradienteX, false);
+document.getElementById("op-gradienteY").addEventListener('click', colorGradienteY, false);
+document.getElementById("op-contraste").addEventListener('click', opchangeContraste, false);
+//op matematicas
+document.getElementById("op-pow").addEventListener('click', opgetPow, false);
+document.getElementById("op-sqrt").addEventListener('click', sqrt, false);
+document.getElementById("op-sine").addEventListener('click', funcionSine, false);
+document.getElementById("op-cos").addEventListener('click', coseno, false);
+document.getElementById("op-tan").addEventListener('click', tan, false);
+document.getElementById("op-add").addEventListener('click', add, false);
+document.getElementById("op-subtract").addEventListener('click', subtract, false);
+document.getElementById("op-multiplicacion").addEventListener('click', multiplicacion, false);
+document.getElementById("op-div").addEventListener('click', div, false);
+//op con imagenes compuestas
+document.getElementById("op-addimg").addEventListener('click', sumaImg, false);
+//op con efectos
+document.getElementById("op-rain").addEventListener('click', rain, false);
+document.getElementById("op-rain2").addEventListener('click', rain2, false);
+//op con texto.
+document.getElementById("op-text").addEventListener('click', textEfects, false);
+//op nuevas 
+document.getElementById("pattern3").addEventListener('click', pattern3Particle, false);
